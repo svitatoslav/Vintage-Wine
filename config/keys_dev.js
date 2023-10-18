d3480c0639bbffe747df3ce0949e0c74e2706746
IvanUA <vanylinss94@gmail.com>
2023-10-18 17:19:29 +0300

    mongoDb in progress

--- config/keys_dev.js
+++ config/keys_dev.js
@@ -1,5 +1,6 @@
 module.exports = {
   mongoURI:
-    'mongodb+srv://vintage_vine:<3U4%29p%3CH4f%2F1A>@vintagevine.wjuqlba.mongodb.net/?retryWrites=true&w=majority',
+    'mongodb+srv://vanylinss94:YnBtG32ePF3cL4kX@vintagevine.wjuqlba.mongodb.net/?retryWrites=true&w=majority',
   secretOrKey: 'random very very secret string',
 };
+
