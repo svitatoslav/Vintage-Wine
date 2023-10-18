76da195a7dc930e9815a2e50cb3e4ae9741a5b6a
IvanUA <vanylinss94@gmail.com>
2023-10-18 13:22:04 +0300

    added backEnd and mongoDB urls

---
+++ config/keys_dev.js
@@ -0,0 +1,5 @@
+module.exports = {
+  mongoURI:
+    'mongodb+srv://vintage_vine:<3U4%29p%3CH4f%2F1A>@vintagevine.wjuqlba.mongodb.net/?retryWrites=true&w=majority',
+  secretOrKey: 'random very very secret string',
+};
