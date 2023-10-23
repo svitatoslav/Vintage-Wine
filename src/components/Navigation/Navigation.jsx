728c746732b299f4667ca6024e862021099f0235
maksymopanasenko <maxpanas008@gmail.com>
2023-10-23 21:58:44 +0200

    add active state for links in mobile menu

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -3,24 +3,28 @@ import PropTypes from 'prop-types';
 import styles from './Navigation.module.scss';
 import { pages } from '../../assets/pages';
 import cn from 'classnames';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 
 
 const Navigation = ({ mobile }) => {
+  const { pathname } = useLocation();
+
+  const links = pages?.map((page, i) => {
+    const path = `/${page.toLowerCase()}`;
+
+    return (
+      <Link key={i}
+        to={path}
+        className={cn(styles.PageLink, {[styles.PageLinkActive]: mobile && pathname === path})}
+      >
+        {page}
+      </Link>
+    );
+  });
+
   return (
     <ul className={cn(styles.Navigation, { [styles.NavigationBurger]: mobile })} data-testid="Navigation">
-      {
-        pages.map((page, i) => {
-          const path = page.toLowerCase();
-          return (
-            <Link key={i}
-              to={`/${path}`}
-              className={styles.PageLink}
-            >
-              {page}
-            </Link>)
-        })
-      }
+      {links}
     </ul>
   );
 }
