0ab671e90a5b1bc34834c43d33aafcfb79dff081
Bohdan Baidan <baidanbv@gmail.com>
2023-10-29 21:14:01 +0100

    remove container component

--- src/pages/Layout/Layout.jsx
+++ src/pages/Layout/Layout.jsx
@@ -1,20 +1,17 @@
-import { Outlet } from "react-router-dom";
-import Header from "../../components/Header/Header";
-import Container from "../../components/Container/Container";
-import Footer from "../../components/Footer/Footer";
+import { Outlet } from 'react-router-dom';
+import Header from '../../components/Header/Header';
+import Footer from '../../components/Footer/Footer';
 
 const Layout = () => {
     return (
         <>
             <Header />
             <main>
-                <Container>
-                    <Outlet />
-                </Container>
-            </main>            
+                <Outlet />
+            </main>
             <Footer />
         </>
     );
-}
- 
+};
+
 export default Layout;
