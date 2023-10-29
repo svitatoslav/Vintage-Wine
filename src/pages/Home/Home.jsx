26f4b5f7a78361e6bbf55d71116db518a9d82e18
Bohdan Baidan <baidanbv@gmail.com>
2023-10-29 20:56:24 +0100

    init

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,17 +1,18 @@
+import React from 'react';
 import Catalog from '../../components/Catalog/Catalog';
 import Collections from '../../components/Collections/Collections';
 import Cover from '../../components/Cover/Cover';
 import History from '../../components/History/History';
 
 const Home = () => {
-    return (
-        <main>
-            <Cover/>
-            <Collections />
-            <History />
-            <Catalog />
-        </main>
-    );
-};
+  return (
+    <>
+      <Cover />
+      <Collections />
+      <History />
+      <Catalog />
+    </>
+  );
+}
 
 export default Home;
