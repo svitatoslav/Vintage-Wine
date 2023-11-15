8c23a65a7761078b86f5afa5773439e5601217fd
Sviatoslav <svatoslavvolk@gmail.com>
2023-11-16 00:40:12 +0100

    Finish article content

--- src/pages/SingleNews/SingleNews.jsx
+++ src/pages/SingleNews/SingleNews.jsx
@@ -1,11 +1,15 @@
 import {useEffect, useState} from "react";
 import {useParams} from "react-router-dom";
 import ArticleContent from "../../components/ArticleContent/ArticleContent";
+import PageTitle from "../../components/Title/PageTitle";
+import Breadcrumbs from "../../components/Breadcrumbs/Breadcrumbs";
+import useBreadcrumbs from "../../hooks/useBreadcrumbs";
 
 
 const SingleNews = () => {
     const {id} = useParams()
     const [article, setArticle] = useState(null)
+    const pathParts = useBreadcrumbs();
 
     useEffect(() => {
          const getArticleById = async () => {
@@ -23,6 +27,8 @@ const SingleNews = () => {
 
     return (
         <div>
+            <PageTitle text="News"/>
+            <Breadcrumbs pathParts={pathParts} />
             <ArticleContent article={article}/>
         </div>
     )
