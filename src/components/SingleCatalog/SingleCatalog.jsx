1359304f8159ed9054dd61a6d1ea69599354583b
Bohdan Baidan <baidanbv@gmail.com>
2023-11-23 21:19:05 +0100

    catalog pages added without btn to cart

--- src/components/SingleCatalog/SingleCatalog.jsx
+++ src/components/SingleCatalog/SingleCatalog.jsx
@@ -1,22 +1,30 @@
 import React from 'react';
-import styles from './SingleCatalog.module.scss';
 import Breadcrumbs from '../Breadcrumbs/Breadcrumbs';
 import useBreadcrumbs from '../../hooks/useBreadcrumbs';
 import { useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
+import ProductCard from '../ProductCard/ProductCard';
 
+import styles from './SingleCatalog.module.scss';
+import Container from './../Container/Container';
 
 const SingleCatalog = () => {
-  const pathParts = useBreadcrumbs();
-  const params = useParams()
-  const products = useSelector((state) => state.products.products)
-  const sortedByCategory = products.filter((product) => product.categories.toLowerCase().includes(params.slug));
-
-  return (
-      <div className={styles.SingleCatalog} data-testid="SingleCatalog">
-        {<Breadcrumbs pathParts={pathParts} />}
-      </div>
-  );
+    const pathParts = useBreadcrumbs();
+    const params = useParams();
+    const products = useSelector((state) => state.products.products);
+    const sortedByCategory = products.filter((product) => product.categories.toLowerCase().includes(params.slug));
+    return (
+        <Container>
+            <div className={styles.SingleCatalog} data-testid="SingleCatalog">
+                {<Breadcrumbs pathParts={pathParts} />}
+                <div className={styles.ProductWrapper}>
+                    {sortedByCategory.map((product) => (
+                        <ProductCard key={product._id} name={product.name} price={product.currentPrice} img={product.productImg} id={product._id} />
+                    ))}
+                </div>
+            </div>
+        </Container>
+    );
 };
 
 export default SingleCatalog;
