4e1e85a8ebdfbd4ad67df406fe18babe906fc294
maksymopanasenko <maxpanas008@gmail.com>
2023-11-24 16:29:00 +0100

    fix modals

--- src/components/SingleExcursion/SingleExcursion.jsx
+++ src/components/SingleExcursion/SingleExcursion.jsx
@@ -5,7 +5,7 @@ import Arrow from './icons/arrow.svg?react';
 import Calendar from './icons/calendar.svg?react';
 import cn from 'classnames';
 import { useDispatch } from 'react-redux';
-import { selectTourAC, toggleModalAC } from '../../redux/reducers/modalWindow-reducer';
+import { selectTourAC, switchModalAC, toggleModalAC } from '../../redux/reducers/modalWindow-reducer';
 
 const SingleExcursion = ({ reverse, orderNum, data }) => {
   const dispatch = useDispatch();
@@ -22,6 +22,7 @@ const SingleExcursion = ({ reverse, orderNum, data }) => {
 
   const handleOpenModal = (title) => {
     dispatch(selectTourAC(title));
+    dispatch(switchModalAC("reservation"));
     dispatch(toggleModalAC());
   }
 
