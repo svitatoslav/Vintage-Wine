bb16edfd9852a0b5d189e0ac3d8a1c92f1e13748
Kostiantyn <kostiantyn254@gmail.com>
2023-11-28 00:22:53 +0200

    add styles to the shares component

--- src/components/Shares/Shares.jsx
+++ src/components/Shares/Shares.jsx
@@ -5,6 +5,7 @@ import {fetchSharesThunk} from "../../redux/reducers/shares-reducer";
 import Container from './../Container/Container';
 import CustomSlider from '../CustomSlider/CustomSlider';
 import SectionTitle from './../Title/SectionTitle';
+import styles from './Shares.module.scss'
 
 const Shares = () => {
     const dispatch = useDispatch();
@@ -16,7 +17,11 @@ const Shares = () => {
         <section>
             <Container>
                 <SectionTitle secText={'Shares'} subText={'Our'} />
-                <CustomSlider sliderArray={shares} type="SHARES" toShow={3} toScroll={1} />
+                {shares && shares.length > 0 ? (
+                    <CustomSlider sliderArray={shares} type="SHARES" toShow={3} toScroll={1} />
+                ) : (
+                    <p className={`${styles.noShares}`}>We don't have any active promotions at the moment, but stay tuned! We are constantly working to create new offers for you.</p>
+                )}
             </Container>
         </section>
     );
